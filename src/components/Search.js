645d662a543b47a92521eec1db2b0d37d20d9eea
Hamza <hamzayousfi2@gmail.com>
2020-11-17 13:29:34 +0100

    updates of the month to the app's code

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,19 +3,19 @@ import { Consumer } from "../context";
 
 export default class Search extends Component {
   state = {
-    trackTitle: ""
+    trackTitle: "",
   };
 
-  handleChange = e => {
+  handleChange = (e) => {
     this.setState({
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,
     });
   };
 
   render() {
     return (
       <Consumer>
-        {value => {
+        {(value) => {
           const { findSongs, lightTheme, light, dark } = value;
           const theme = lightTheme ? light : dark;
           return (
@@ -24,7 +24,7 @@ export default class Search extends Component {
               style={{ color: theme.Bcolor, background: theme.background }}
             >
               <h1 className="display-4 text-center">
-                <i className="fas fa-music" /> Search For A Song
+                <i className="fas fa-music" /> Search For Songs
               </h1>
               <p className="lead text-center">Get The Lyrics For Any Song</p>
               <div className="form-group">
@@ -44,11 +44,13 @@ export default class Search extends Component {
                     background: theme.Bcolor,
                     color: theme.Bbackground,
                     border: "solid",
-                    borderRadius: "5rem"
+                    borderRadius: "5rem",
                   }}
                   className="btn btn-lg mt-3 display-flex justify-content-around "
                   name="Artist"
-                  onClick={e => findSongs(e.target.name, this.state.trackTitle)}
+                  onClick={(e) =>
+                    findSongs(e.target.name, this.state.trackTitle)
+                  }
                 >
                   <i class="fas fa-user-circle mr-1" /> Search By Artist{" "}
                 </button>
@@ -57,11 +59,13 @@ export default class Search extends Component {
                     background: theme.Bcolor,
                     color: theme.Bbackground,
                     border: "solid",
-                    borderRadius: "5rem"
+                    borderRadius: "5rem",
                   }}
                   className="btn  btn-lg mt-3"
                   name="Song"
-                  onClick={e => findSongs(e.target.name, this.state.trackTitle)}
+                  onClick={(e) =>
+                    findSongs(e.target.name, this.state.trackTitle)
+                  }
                 >
                   <i className="fas fa-music" /> Search By Song{" "}
                 </button>
